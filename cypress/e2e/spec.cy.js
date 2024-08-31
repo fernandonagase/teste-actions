9907d9c8e52076d1e622b65b7b8114fcf4efbffb
Fernando Nagase <nagase.fernando@gmail.com>
2024-08-30 22:36:56 -0300

    automatizar testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,8 @@
 describe('Incrementa ao clicar no botao', () => {
   it('Visits the Kitchen Sink', () => {
-    cy.visit('http://127.0.0.1:5173/')
+    cy.visit('http://127.0.0.1:9001/')
     const counter = cy.contains('count')
     counter.click()
-
     counter.should('have.text', 'count is 1')
   })
 })
